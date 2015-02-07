f4ebc5d485ca5b51a88f339372e3e97ebae7cfc2
ladg340 <degracia.laura@yahoo.com>
2015-02-07 17:54:40 -0300

    Course Project 1 - Plot 1

---
+++ plot1.R
@@ -0,0 +1,7 @@
+all_data<-read.csv("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",nrows=2075259,check.names=F,stringsAsFactors=F,comment.char="",quote="\"")
+all_data$Date<-as.Date(all_data$Date,format="%d/%m/%Y")
+data<-subset(all_data,subset=(Date>="2007-02-01"&Date<="2007-02-02"))
+head(data)
+with(data,hist(Global_active_power,xlab="Global Active Power (kilowatts)",main="Global Active Power",cex.main=0.8,col="red",cex.axis=0.8,cex.lab=0.8))
+dev.copy(png,height=480,width=480,units="px",file="plot1.png")
+dev.off()
